56b14e30270b3fb346e80a227b2274d29b744987
Denize Mansoni <denizedtmc@gmail.com>
2025-09-03 22:27:19 -0300

    atualizando e estilizando o loginpage com responsividade, grid e flexbox

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,12 +4,9 @@ import logo from '../assets/logo-estaciona-ai.png';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,31 +17,35 @@ function LoginPage() {
   return (
     <div className="page-wrapper">
       <div className="login-container">
-      <img src={logo} alt="Logo Estaciona Aí" className="logo" />
-      <h2>Estaciona Aí</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label>Senha:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Entrar</button>
-        <div className="signup-link">
-  <span>Não tem cadastro? </span>
-  <Link to="/cadastro">Clique aqui</Link>
-</div>
-</form>
-    </div>
+        <img src={logo} alt="Logo Estaciona Aí" className="logo" />
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit} className="form-grid">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <label>Senha:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button type="submit">Entrar</button>
+
+          <div className="signup-link">
+            <span>Não tem cadastro? </span>
+            <Link to="/cadastro">Clique aqui</Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
+
 export default LoginPage;
